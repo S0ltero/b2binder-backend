9e7f57704a07bb016d399469dda2120ef3a0bca5
Daikku <vadess2013@mail.ru>
2022-03-19 13:40:55 +0300

    Added to the router.urls paths UserViewSet

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+from api.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include(router.urls)),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
