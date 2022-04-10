90b8fbb005105d9773bdf0adf6f85e83d96adf35
S0ltero <AlexsSobko61@gmail.com>
2022-04-10 16:50:53 +0300

    Add `IsOwner` permission - Check user permission to object

--- api/permissions.py
+++ api/permissions.py
@@ -1,2 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
+
+User = get_user_model()
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
