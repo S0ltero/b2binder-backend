5a568269b9480bf113422ce71095c6f6703198c3
S0ltero <AlexsSobko61@gmail.com>
2022-04-19 11:49:53 +0300

    Add `templates` to list of dirs

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,7 @@ MEDIA_URL = "/media/"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
