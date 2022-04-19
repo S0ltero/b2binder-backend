08026081df3752e6eee51ae1825a0ba702274879
S0ltero <AlexsSobko61@gmail.com>
2022-04-19 11:41:32 +0300

    Add dockstrings to all used endpoints in project

--- api/views.py
+++ api/views.py
@@ -51,6 +51,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=False, url_name="me/likes/to", url_path="me/likes/to",
             serializer_class=UserSerializer)
     def likes_to(self, request, *args, **kwargs):
+        """
+        Получение списка пользователей которых я оценил
+        """
         instance = self.request.user
         likes = instance.likes_to.all()
         serializer = self.serializer_class(likes, many=True)
@@ -59,6 +62,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=False, url_name="me/likes/from", url_path="me/likes/from",
             serializer_class=UserSerializer)
     def likes_from(self, request, *args, **kwargs):
+        """
+        Получение списка пользователей оценивших меня
+        """
         instance = self.request.user
         likes = instance.likes_from.all()
         serializer = self.serializer_class(likes, many=True)
@@ -67,6 +73,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=False, url_name="me/likes/projects", url_path="me/likes/projects",
             serializer_class=ProjectsSerializer)
     def like_projects(self, request, *args, **kwargs):
+        """
+        Получение списка оцененных мной проектов
+        """
         instance = self.request.user
         likes = instance.like_projects.all()
         serializer = self.serializer_class(likes, many=True)
@@ -75,6 +84,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=False, url_name="me/projects/likes", url_path="me/projects/likes",
             serializer_class=UserSerializer)
     def projects_likes(self, request, *args, **kwargs):
+        """
+        Получение списка пользователей оценивших мои проекты
+        """
         instance = self.request.user
         user_ids = instance.projects.values_list("likes", flat=True)
         likes = CustomUser.objects.filter(id__in=user_ids)
@@ -84,6 +96,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=True, methods=['post'], url_name='subscribe', url_path='subscribe',
             serializer_class=UserSubscribeSerializer)
     def subscribe(self, request, id=None):
+        """
+        Добавление подписки на пользователя с `id`
+        """
         data = request.data.copy()
         data['subscriber'] = self.request.user.id
         data['subscription'] = id
@@ -98,6 +113,9 @@ class UserViewSet(DjoserUserViewSet):
     @action(detail=False, url_name='me/subscribers/', url_path='me/subscribers',
             serializer_class=UserSerializer)
     def subscribers(self, request, *args, **kwargs):
+        """
+        Получение списка моих подписчиков
+        """
         instance = self.request.user
         subscribers = instance.subscribers.all()
         serializer = self.serializer_class(subscribers, many=True)
@@ -138,6 +156,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
         return super().get_permissions()
 
     def list(self, request, *args, **kwargs):
+        """
+        Получение списка всех проектов
+        """
         try:
             projects = self.get_queryset()
         except Project.DoesNotExist:
@@ -146,6 +167,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
+        """
+        Получение проекта с указанным `pk`
+        """
         try:
             project = self.queryset.objects.get(id=pk)
         except Project.DoesNotExist:
@@ -153,7 +177,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
         serializer = self.serializer_class(project)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def update(self, request, pk):
+        """
+        Обновление проекта с указанным `pk`
+        """
         project = self.get_object()
         serializer = self.serializer_class(project, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=False):
@@ -163,6 +189,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def create(self, request, *args, **kwargs):
+        """
+        Создание проекта
+        """
         data = request.data.copy()
         data['user'] = request.user.id
 
@@ -174,6 +203,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk):
+        """
+        Удаление проекта с указанным `pk`
+        """
         project = self.get_object()
         project.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -181,6 +213,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
     @action(detail=True, url_name='detail', url_path='detail',
             serializer_class=ProjectsDetailSerializer)
     def project_detail(self, request, pk=None):
+        """
+        Получение детальной информации о проекте с указанным `pk`
+        """
         project = self.get_object()
         serializer = self.serializer_class(project)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -188,6 +223,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
     @action(detail=True, methods=['post'], url_name='likes', url_path='likes',
             serializer_class=ProjectLikeSerializer)
     def project_likes(self, request, pk=None):
+        """
+        Добавление оценки от пользователя `request.user` к проекту с указанным `pk`
+        """
         data = request.data.copy()
         data["project"] = pk
         data["user"] = self.request.user.id
@@ -202,6 +240,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
     @action(detail=True, methods=['post'], url_name='comments', url_path='comments',
             serializer_class=ProjectCommentSerializer)
     def comments(self, request, pk=None):
+        """
+        Добавление комментария от пользователя `request.user` к проекту с указанным `pk`
+        """
         data = request.data.copy()
         data["project"] = pk
         data["user"] = self.request.user.id
@@ -230,6 +271,9 @@ class ProjectViewSet(viewsets.GenericViewSet):
     @action(detail=True, methods=['post'], url_name='offers', url_path='offers',
             serializer_class=ProjectOfferSerializer)
     def offers(self, request, pk=None):
+        """
+        Добавление предложения от пользователя `request.user` к проекту с указанным `pk`
+        """
         data = request.data.copy()
         data['user'] = self.request.user.id
         data['project'] = pk
@@ -242,8 +286,10 @@ class ProjectViewSet(viewsets.GenericViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class CallbackAPIView(CreateAPIView):
+    """
+    Добавление обратной связи
+    """
     queryset = Callback.objects.all()
     serializer_class = CallbackCreateSerializer
     permission_classes = (AllowAny,)
