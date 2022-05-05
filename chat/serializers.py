c7a2c3b9e87ae7147443a51645f0acfa1850d102
S0ltero <AlexsSobko61@gmail.com>
2022-05-05 12:03:03 +0300

    Add `ChatSerializer` serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -16,3 +16,21 @@ class MessageSerializer(serializers.ModelSerializer):
 
     def get_created_at_formatted(self, obj:Message):
         return obj.timestamp.strftime("%d-%m-%Y %H:%M:%S")
+
+class ChatSerializer(serializers.ModelSerializer):
+    last_message = serializers.SerializerMethodField()
+    recepient = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Chat
+        fields = ("pk", "last_message", "recepient")
+        depth = 1
+        read_only_fields = ("last_message", "recepient")
+
+    def get_last_message(self, obj: Chat):
+        return MessageSerializer(obj.messages.order_by("timestamp").last()).data
+
+    def get_recepient(self, obj: Chat):
+        recepient = obj.members.exclude(pk=self.context["scope"]["user"].id).first()
+        serializer = UserSerializer(recepient)
+        return serializer.data
