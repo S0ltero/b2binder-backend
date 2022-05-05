72abe1a311a0b53540e47f1416fdf4759aadabde
S0ltero <AlexsSobko61@gmail.com>
2022-05-05 12:01:38 +0300

    Add `Message` model

--- chat/models.py
+++ chat/models.py
@@ -10,3 +10,17 @@ class Chat(models.Model):
     class Meta:
         verbose_name = "Чат"
         verbose_name_plural = "Чаты"
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="Сообщение")
+    timestamp = models.DateTimeField(verbose_name="Дата публикации", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+
+    def __str__(self) -> str:
+        return self.author.email
