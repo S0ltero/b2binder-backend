3aee3cce49f69f54f8269118b5735a059441392b
S0ltero <AlexsSobko61@gmail.com>
2022-05-05 12:12:29 +0300

    Implement support of `channel` app

--- config/asgi.py
+++ config/asgi.py
@@ -2,6 +2,18 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
+
+from chat import routing, middleware
+
+application = ProtocolTypeRouter({
+  "http": django_asgi_app,
+  "websocket": middleware.TokenAuthMiddleware(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
