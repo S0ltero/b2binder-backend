71db4db1b3f6d2a72f07b9be379b845e9322a8ba
S0ltero <AlexsSobko61@gmail.com>
2022-05-05 12:01:22 +0300

    Add `Chat` model

--- chat/models.py
+++ chat/models.py
@@ -2,3 +2,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+
+
+class Chat(models.Model):
+    members = models.ManyToManyField(User, related_name="chats")
+
+    class Meta:
+        verbose_name = "Чат"
+        verbose_name_plural = "Чаты"
