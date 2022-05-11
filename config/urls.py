07a0d177eb4ce9bf301e45ec2302e7c5723f754d
S0ltero <AlexsSobko61@gmail.com>
2022-05-11 12:26:29 +0300

    Remove `drf_yasg` app from project

--- config/urls.py
+++ config/urls.py
@@ -3,18 +3,11 @@ from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 
 from rest_framework.routers import DefaultRouter
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 from api.views import UserViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
-
-schema_view = get_schema_view(
-    openapi.Info(title="B2binder API", default_version="v1"),
-    public=True,
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,9 +15,4 @@ urlpatterns = [
     path("api/auth/", include(router.urls)),
     path("api/auth/", include("djoser.urls.authtoken")),
     path("chat/", include("chat.urls")),
-    path(
-        "redoc/",
-        login_required(schema_view.with_ui("redoc", cache_timeout=0)),
-        name="schema-redoc",
-    ),
 ]
