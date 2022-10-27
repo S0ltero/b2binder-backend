a3849aa77cfaab4fb945385c379853ee86feda09
S0ltero <AlexsSobko61@gmail.com>
2022-10-27 19:49:23 +0300

    Add `HasSubscription` permission

--- api/permissions.py
+++ api/permissions.py
@@ -7,3 +7,9 @@ User = get_user_model()
 class IsOwner(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class HasSubscription(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.is_staff or user.has_subscription
